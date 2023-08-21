df4c0d52a92e6ed4a640f8cfd7c427580897d94d
phamduc1823 <phamduc1823@gmail.com>
2023-08-21 16:56:08 +0700

    fix textarea from create & wrap text form list

--- app/Filament/Resources/Quiz/ContestQuestionResource.php
+++ app/Filament/Resources/Quiz/ContestQuestionResource.php
@@ -53,24 +53,29 @@ class ContestQuestionResource extends Resource
                     ->label('Title Origin')
                     ->searchable()
                     ->sortable()
-                    ->toggleable(),
+                    ->toggleable()
+                    ->wrap(),
 
                 Tables\Columns\ImageColumn::make('image')
                     ->label('Image')
-                    ->toggleable(),
+                    ->toggleable()
+                    ->visibleFrom('md'),
 
                 Tables\Columns\TextColumn::make('title_extra')
                     ->label('Title Extra')
                     ->searchable()
                     ->sortable()
-                    ->toggleable(),
+                    ->toggleable()
+                    ->visibleFrom('md')
+                    ->wrap(),
 
                 Tables\Columns\IconColumn::make('is_active')
                     ->label('Is active')
                     ->searchable()
                     ->boolean()
                     ->sortable()
-                    ->toggleable(),
+                    ->toggleable()
+                    ->visibleFrom('md'),
             ])
             ->filters([
                 Tables\Filters\TernaryFilter::make('is_active')
@@ -169,7 +174,7 @@ class ContestQuestionResource extends Resource
                 ->inline(false)
                 ->default(true),
 
-            Forms\Components\MarkdownEditor::make('title_origin')
+            Forms\Components\Textarea::make('title_origin')
                 ->columnSpan('full')
                 ->placeholder('Enter question title here')
                 ->required(),
@@ -179,11 +184,11 @@ class ContestQuestionResource extends Resource
                 ->image()
                 ->columnSpan('full'),
 
-            Forms\Components\MarkdownEditor::make('title_extra')
+            Forms\Components\Textarea::make('title_extra')
                 ->placeholder('Add more questions if any')
                 ->columnSpan('full'),
 
-            Forms\Components\MarkdownEditor::make('explain')
+            Forms\Components\Textarea::make('explain')
                 ->placeholder('Explain the answer if any')
                 ->columnSpan('full'),
         ];
