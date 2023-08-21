914428272968f448dffe0c5557901f5c590a0c03
Tống Văn Đức <tongduc315@gmail.com>
2023-08-21 22:58:55 +0700

    Fix seed for server

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,23 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Filament\Resources\Shop\OrderResource;
-use App\Models\Address;
-use App\Models\Blog\Author;
-use App\Models\Blog\Category as BlogCategory;
-use App\Models\Blog\Post;
-use App\Models\Comment;
 use App\Models\Quiz\Contest;
 use App\Models\Quiz\ContestAnswer;
 use App\Models\Quiz\ContestQuestion;
 use App\Models\Quiz\ContestResult;
-use App\Models\Shop\Brand;
-use App\Models\Shop\Category as ShopCategory;
-use App\Models\Shop\Customer;
-use App\Models\Shop\Order;
-use App\Models\Shop\OrderItem;
-use App\Models\Shop\Payment;
-use App\Models\Shop\Product;
 use App\Models\User;
 use Closure;
 use Filament\Notifications\Actions\Action;
