da0b4243a1a00e7f6573a01e33869bd0ebac4a09
phamduc1823 <phamduc1823@gmail.com>
2023-09-07 10:55:53 +0700

    synchronous done

--- app/Filament/Resources/Quiz/ExamQuestionResource/Pages/CreateExamQuestion.php
+++ app/Filament/Resources/Quiz/ExamQuestionResource/Pages/CreateExamQuestion.php
@@ -9,4 +9,16 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateExamQuestion extends CreateRecord
 {
     protected static string $resource = ExamQuestionResource::class;
+
+    protected function sendCreatedNotificationAndRedirect(bool $shouldCreateAnotherInsteadOfRedirecting = true): void
+    {
+        $this->getCreatedNotification()?->send();
+
+        $this->redirect($this->getRedirectUrl());
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return  $this->getResource()::getUrl('create');
+    }
 }
