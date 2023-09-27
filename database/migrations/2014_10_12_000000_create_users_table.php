1f9d5561f149b2661689ee961a9b7fc31b5a5465
Tống Văn Đức <tongduc315@gmail.com>
2023-09-27 21:54:44 +0700

    Fix type user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class() extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('type_user')->default(TYPE_USER_STUDENT);
+            $table->string('type_user', 50)->default(TYPE_USER_STUDENT);
             $table->rememberToken();
             $table->timestamps();
         });
