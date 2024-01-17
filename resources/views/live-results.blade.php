9d6ac6576bc1d3aeb2389b06e6c8d56dba9ab072
dinhtv7 <nguyenduyquangvinh2906@gmail.com>
2024-01-17 21:06:02 +0700

    merge realtime code

--- resources/views/live-results.blade.php
+++ resources/views/live-results.blade.php
@@ -15,9 +15,6 @@
 <div class="container min-h-screen pb-10 bg-[url('./assets/gif/bg.gif')] my-0 mx-auto">
     <div class="content-top grid grid-cols-3 border-gray-300  h-[50px] py-3">
         <div class="text-left ml-3 mt-2">
-            {{--            <a href="#"--}}
-            {{--               class="py-2.5 px-5 mr-2 mb-2 text-sm font-bold text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-300 focus:z-10 focus:ring-4 focus:ring-gray-200">--}}
-            {{--                <span id="examName">Quizz</span></a>--}}
         </div>
         <div class="text-center mx-auto mt-2">
             <div class="bg-white w-[100%] float-left mt-[-10px]
@@ -38,28 +35,8 @@
             </div>
         </div>
         <div class="text-right mr-3 mt-2">
-            {{--            <a href="#"--}}
-            {{--               class="py-2.5 px-5 mr-2 mb-2 text-sm font-bold text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-300 focus:z-10 focus:ring-4 focus:ring-gray-200">--}}
-            {{--                Tạm dừng</a>--}}
-            {{--            <a href="#"--}}
-            {{--               class="py-2.5 px-5 mr-2 mb-2 text-sm font-bold text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-300 focus:z-10 focus:ring-4 focus:ring-gray-200">--}}
-            {{--                Kết thúc</a>--}}
         </div>
     </div>
-
-    {{--    <div id="content-progess" class="content-main bg-gray-700 rounded-lg h-12 w-[80%] mx-auto mt-[7%] flex flex-col-3">--}}
-    {{--        <div class="w-[50%] rounded-lg h-12 dark:bg-gray-700">--}}
-    {{--            <div id="progressBar1" class="bg-green-400 h-10 rounded-lg mt-1 ml-1" style="width: 0"></div>--}}
-    {{--        </div>--}}
-    {{--        <div class="rounded-full w-20 z-0 h-20 bg-gray-50 mt-[-16px] ring-8 ring-gray-300">--}}
-    {{--            <p class="font-semibold text-xs justify-center mt-5 mx-2 text-center">--}}
-    {{--                <strong id="percentage1" class="text-[17px]">0%</strong><br>--}}
-    {{--                độ chính xác</p>--}}
-    {{--        </div>--}}
-    {{--        <div class="w-[50%] rounded-lg h-12 dark:bg-gray-700">--}}
-    {{--            <div id="progressBar2" class="bg-red-400 h-10 rounded-lg mt-1 mr-1 float-right" style="width: 0"></div>--}}
-    {{--        </div>--}}
-    {{--    </div>--}}
 
     <div class="relative overflow-x-auto mt-[8%] w-[80%] mx-auto rounded-lg">
         <table class="w-full text-sm text-left text-gray-500">
@@ -96,74 +73,6 @@
             </tr>
             </thead>
             <tbody id="table-body">
-            <!-- <tr class="bg-white border-b errors-row">
-                <th scope="row" class="px-6 py-4 w-[10%] font-medium text-gray-900 whitespace-nowrap">
-                    1
-                </th>
-                <td class="px-6 py-4">
-                    Silver
-                </td>
-                <td class="px-6 py-4">
-                    12000
-                </td>
-                <td class="px-6 py-4 w-[50%]">
-                    <div class="w-[100%] bg-[#f0f0f0] rounded-lg h-12 inline-flex">
-                        <div class="bg-green-400 inline-flex ml-1.5 mt-1 h-10 rounded-l-lg" style="width: 50%">
-                            <svg class="mt-2.5 ml-2 w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 17 20">
-                                <path d="M7.958 19.393a7.7 7.7 0 0 1-6.715-3.439c-2.868-4.832 0-9.376.944-10.654l.091-.122a3.286 3.286 0 0 0 .765-3.288A1 1 0 0 1 4.6.8c.133.1.313.212.525.347A10.451 10.451 0 0 1 10.6 9.3c.5-1.06.772-2.213.8-3.385a1 1 0 0 1 1.592-.758c1.636 1.205 4.638 6.081 2.019 10.441a8.177 8.177 0 0 1-7.053 3.795Z"/>
-                            </svg>
-                            <p class="pl-0.5 pt-2 font-bold text-white">0</p>
-                        </div>
-                        <div class="bg-red-400 mt-1 h-10 rounded-r-lg" style="width: 10%"></div>
-                    </div>
-                </td>
-            </tr> -->
-            <!-- bg-green-200 -->
-            <!-- <tr class="bg-white border-b success-row">
-                <th scope="row" class="px-6 py-4 w-[10%] font-medium text-gray-900 whitespace-nowrap">
-                    2
-                </th>
-                <td class="px-6 py-4">
-                    Omega
-                </td>
-                <td class="px-6 py-4">
-                    5600
-                </td>
-                <td class="px-6 py-4 w-[50%]">
-                    <div class="w-[100%] bg-[#f0f0f0] rounded-lg h-12 inline-flex">
-                        <div class="bg-green-400 inline-flex ml-1.5 mt-1 h-10 rounded-l-lg" style="width: 20%">
-                            <svg class="mt-2.5 ml-2 w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 17 20">
-                                <path d="M7.958 19.393a7.7 7.7 0 0 1-6.715-3.439c-2.868-4.832 0-9.376.944-10.654l.091-.122a3.286 3.286 0 0 0 .765-3.288A1 1 0 0 1 4.6.8c.133.1.313.212.525.347A10.451 10.451 0 0 1 10.6 9.3c.5-1.06.772-2.213.8-3.385a1 1 0 0 1 1.592-.758c1.636 1.205 4.638 6.081 2.019 10.441a8.177 8.177 0 0 1-7.053 3.795Z"/>
-                            </svg>
-                            <p class="pl-0.5 pt-2 font-bold text-white">0</p>
-                        </div>
-                        <div class="bg-red-400 mt-1 h-10 rounded-r-lg" style="width: 10%"></div>
-                    </div>
-                </td>
-            </tr> -->
-            <!-- bg-red-200 -->
-            <!-- <tr class="bg-white border-b">
-                <th scope="row" class="px-6 py-4 w-[10%] font-medium text-gray-900 whitespace-nowrap">
-                    3
-                </th>
-                <td class="px-6 py-4">
-                    Lisa
-                </td>
-                <td class="px-6 py-4">
-                    1600
-                </td>
-                <td class="px-6 py-4 w-[50%]">
-                    <div class="w-[100%] bg-[#f0f0f0] rounded-lg h-12 inline-flex">
-                        <div class="bg-green-400 inline-flex ml-1.5 mt-1 h-10 rounded-l-lg" style="width: 10%">
-                            <svg class="mt-2.5 ml-2 w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 17 20">
-                                <path d="M7.958 19.393a7.7 7.7 0 0 1-6.715-3.439c-2.868-4.832 0-9.376.944-10.654l.091-.122a3.286 3.286 0 0 0 .765-3.288A1 1 0 0 1 4.6.8c.133.1.313.212.525.347A10.451 10.451 0 0 1 10.6 9.3c.5-1.06.772-2.213.8-3.385a1 1 0 0 1 1.592-.758c1.636 1.205 4.638 6.081 2.019 10.441a8.177 8.177 0 0 1-7.053 3.795Z"/>
-                            </svg>
-                            <p class="pl-0.5 pt-2 font-bold text-white">0</p>
-                        </div>
-                        <div class="bg-red-400 mt-1 h-10 rounded-r-lg" style="width: 50%"></div>
-                    </div>
-                </td>
-            </tr> -->
             </tbody>
         </table>
     </div>
